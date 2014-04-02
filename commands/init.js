072167382752359696941883e40b029f28698312
Scott Mebberson <scott@scottmebberson.com>
2014-04-02 12:42:09 +1030

    Using async to control flow for better error reporting.

--- commands/init.js
+++ commands/init.js
@@ -2,62 +2,88 @@
 var os = require('os'),
 	path = require('path'),
 	init = require('../lib/init'),
-	chalk = require('chalk');
+	chalk = require('chalk')
+	async = require('async');
 
 module.exports = function (argv) {
 
 	// let's parse the url
-	var url;
+	var url,
+		folder;
 
-	init.parseURL(argv.args.url, function (err, parsedURL) {
+	async.series([
+
+		function (cb) {
+
+			init.parseURL(argv.args.url, function (err, parsedURL) {
 		
-		if (err) {
-			return console.error(chalk.red('\n' + err));
+				if (err) {
+					console.error(chalk.red('\n' + err));
+					return cb(err);
+				}
+
+				url = parsedURL;
+
+				cb();
+
+			});
+
+		},
+
+		function (cb) {
+
+			init.parseFolder(argv.args.path, function (err, parsedFolder) {
+
+				if (err) {
+					console.error(chalk.red('\n' + err));
+					return cb(err);
+				}
+
+				folder = parsedFolder;
+
+				cb();
+
+			});
+
 		}
 
-		url = parsedURL;
+	],
 
-	});
-
-	// let's parse the local folder
-	var folder;
-
-	init.parseFolder(argv.args.path, function (err, parsedFolder) {
-
-		if (err) {
-			return console.error(chalk.red('\n' + err));
-		}
-
-		folder = parsedFolder;
-
-	});
-
-
-	// now that we have everything we need, let's:
-	// 1. clone the directory
-	// 2. remove the .git folder
-
-	console.log('\n' + chalk.cyan('Cloning \'%s\' into \'%s\'...'), url, folder);
-
-	// 1. clone the directory
-	init.clone(url, folder, function (err, location) {
-
-		if (err) {
-			return console.error(chalk.red('\n' + err));
-		}
-
-		// 2. degit baby
-		init.degit(path.join(folder, '.git'), function (err) {
+		function (err, results) {
 
 			if (err) {
-				return console.error(chalk.red('\n' + err));
+				return;
 			}
 
-			// we're all done
-			return console.log(chalk.green('All done.\n'));
+			// now that we have everything we need, let's:
+			// 1. clone the directory
+			// 2. remove the .git folder
 
-		});
+			console.log('\n' + chalk.cyan('Cloning \'%s\' into \'%s\'...'), url, folder);
 
-	});
+			// 1. clone the directory
+			init.clone(url, folder, function (err, location) {
+
+				if (err) {
+					return console.error(chalk.red('\n' + err));
+				}
+
+				// 2. degit baby
+				init.degit(path.join(folder, '.git'), function (err) {
+
+					if (err) {
+						return console.error(chalk.red('\n' + err));
+					}
+
+					// we're all done
+					return console.log(chalk.green('All done.\n'));
+
+				});
+
+			});
+
+		}
+
+	);
 
 };
